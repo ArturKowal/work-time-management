1d3d4a5c4c2e7de7322d59a14b59650603b49974
Artur <artur@MacBook-Air-Artur.local>
2020-02-11 17:52:59 +0100

    Own profile user - fix

--- projekt/src/aplikacja/account/models.py
+++ projekt/src/aplikacja/account/models.py
@@ -3,30 +3,34 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class MyAccountManager(BaseUserManager):
-	def create_user(self, email, name,last_name, password=None):
+	def create_user(self, email ,ident, first_name ,last_name ,password=None):
 		if not email:
 			raise ValueError('Users must have an email address')
-		if not name:
+		if not first_name:
 			raise ValueError('Users must have a name')
 		if not last_name:
 			raise ValueError('Users must have a last name')
+		if not ident:
+			raise ValueError('Users must have a last ident')
 
 		user = self.model(
 			email=self.normalize_email(email),
-			name=name,
+			first_name=first_name,
 			last_name=last_name,
+			ident=ident,
 		)
 
 		user.set_password(password)
 		user.save(using=self._db)
 		return user
 
-	def create_superuser(self, email, name,last_name, password):
+	def create_superuser(self, ident ,email, first_name, last_name, password):
 		user = self.create_user(
 			email=self.normalize_email(email),
 			password=password,
-			name=name,
+			first_name=first_name,
 			last_name=last_name,
+			ident=ident,
 		)
 		user.is_admin = True
 		user.is_staff = True
@@ -38,8 +42,10 @@ class MyAccountManager(BaseUserManager):
 class Account(AbstractBaseUser):
 	email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
 	ident					= models.PositiveIntegerField(default=1234, validators=[MinValueValidator(1000), MaxValueValidator(9999)],unique=True)
-	name 					= models.CharField(max_length=30, unique=False)
+	first_name 				= models.CharField(max_length=30, unique=False)
 	last_name 				= models.CharField(max_length=30, unique=False)
+	position				= models.CharField(verbose_name="Stanowisko ",max_length=40,unique=False,blank=True)
+	stake					= models.FloatField(verbose_name="Stawka ",unique=False,default=0)
 	date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
 	last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
 	is_admin				= models.BooleanField(default=False)
@@ -49,12 +55,16 @@ class Account(AbstractBaseUser):
 
 
 	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['name','last_name']
+	REQUIRED_FIELDS = ['first_name','last_name','ident']
 
 	objects = MyAccountManager()
 
+	class Meta:
+		verbose_name="Konto"
+		verbose_name_plural="Konta"
+
 	def __str__(self):
-		return self.name#, str(self.last_name), str(self.email)
+		return self.first_name + ' ' + self.last_name
 
 	# For checking permissions. to keep it simple all admin have ALL permissons
 	def has_perm(self, perm, obj=None):
